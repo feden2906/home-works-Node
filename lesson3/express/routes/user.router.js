483eff99226fa11987128925b70c8d724c18fbe9
YuraChicalo <47437645+YuraChicalo@users.noreply.github.com>
2021-09-07 21:49:08 +0300

    Delete lesson3 directory

--- lesson3/express/routes/user.router.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { getAllUsers, getUserByID, createUser } = require('../controllers/user.controller');
-
-router.get('/', getAllUsers);
-router.post('/', createUser);
-router.get('/:user_id', getUserByID);
-
-module.exports = router;
